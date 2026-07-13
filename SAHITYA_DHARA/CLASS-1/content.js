d67ae16e75764c6f76723afd174f4d34b81d031f
happysangitanms <happysangitanms@gmail.com>
2026-07-14 00:28:20 +0530

    Update content.js

--- SAHITYA_DHARA/CLASS-1/content.js
+++ SAHITYA_DHARA/CLASS-1/content.js
@@ -8,7 +8,7 @@ const CLASS_NAME = "school9";
 
 const MEDIA_CONFIG = {
     audio1: "https://audio.evidya.de/S9SCP1P1-3.mp3",
-    video:  "https://www.youtube.com/embed/jyNYTTROtU4?playsinline=1&rel=0&modestbranding=1&autoplay=1",
+    video:  "https://www.youtube.com/embed/kH1rc-Y0z0o?playsinline=1&rel=0&modestbranding=1&autoplay=1",
     pdf:    "https://audio.evidya.de/S9SCP1P1-3.pdf"
 };
 
