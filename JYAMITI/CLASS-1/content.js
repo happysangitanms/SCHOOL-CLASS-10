a5f5919806ad29fc662b3a8fe71f88ce0940e3ac
happysangitanms <happysangitanms@gmail.com>
2026-07-14 07:46:07 +0530

    Update content.js

--- JYAMITI/CLASS-1/content.js
+++ JYAMITI/CLASS-1/content.js
@@ -8,7 +8,7 @@ const CLASS_NAME = "school9";
 
 const MEDIA_CONFIG = {
     audio1: "https://audio.evidya.de/S9MTG1P1-2.mp3",
-    video:  "https://www.youtube.com/embed/qU8IfegijAo?playsinline=1&rel=0&modestbranding=1&autoplay=1",
+    video:  "https://www.youtube.com/embed/bJFHdrKDmpM?playsinline=1&rel=0&modestbranding=1&autoplay=1",
     pdf:    "https://audio.evidya.de/S9MTG1P1-2.pdf"
 };
 
