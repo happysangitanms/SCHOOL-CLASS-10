58f6ef8b222708c040eab6a83912eb4800dd7d63
happysangitanms <happysangitanms@gmail.com>
2026-07-21 15:32:32 +0530

    Add configuration for class and media settings

--- ODIA_BYAKARANA/CLASS-2/config.js
+++ ODIA_BYAKARANA/CLASS-2/config.js
@@ -1,1 +1,14 @@
+const CLASS_MODE = "practice";
 
+const APP_CONFIG = {
+    sreni: "ନବମ",
+    subject: "ଓଡ଼ିଆ ବ୍ୟାକରଣ",
+    classCode: "ଓଡ଼ିଆ ବ୍ୟାକରଣ: ପ୍ରଥମ ଅଧ୍ୟାୟ (ପୃଷ୍ଠା 1)"
+};
+
+
+const CLASS_NAME = "school9";
+
+const MEDIA_CONFIG = {
+    summaryPdf: "https://audio.evidya.de/S9FLOG1P1S.pdf"
+};
