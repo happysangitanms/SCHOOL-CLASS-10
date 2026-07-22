14dbbc75bc8adff5aad05a7eb8c595d31d2e39eb
happysangitanms <happysangitanms@gmail.com>
2026-07-23 02:30:02 +0530

    Publish config.js with video ID for JOB-42

---
+++ SSH/CLASS-1/config.js
@@ -0,0 +1,10 @@
+const APP_CONFIG = {
+    sreni: "ଦଶମ",
+    subject: "ଇତିହାସ"
+};
+const CLASS_NAME = "school10";
+const MEDIA_CONFIG = {
+    audio1: "https://audio.evidya.de/1SSH1.mp3",
+    video:  "https://www.youtube.com/embed/SAVJxFk0Ccg?playsinline=1&rel=0&modestbranding=1&autoplay=1",
+    pdf:    "https://audio.evidya.de/1SSH1.pdf"
+};
