520cc001af666b8596386bfabace22ea23fddaaa
happysangitanms <happysangitanms@gmail.com>
2026-07-23 13:12:43 +0530

    Publish config.js with video ID for JOB-76

---
+++ MTA/CLASS-1/config.js
@@ -0,0 +1,10 @@
+const APP_CONFIG = {
+    sreni: "ଦଶମ",
+    subject: "ବୀଜଗଣିତ"
+};
+const CLASS_NAME = "school10";
+const MEDIA_CONFIG = {
+    audio1: "https://audio.evidya.de/1MTA1.mp3",
+    video:  "https://www.youtube.com/embed/KlKsxJiBNwk?playsinline=1&rel=0&modestbranding=1&autoplay=1",
+    pdf:    "https://audio.evidya.de/1MTA1.pdf"
+};
