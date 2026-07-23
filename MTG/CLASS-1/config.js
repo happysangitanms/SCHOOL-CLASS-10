96ffc52465d8fd30dd0108ffab41ed3b3c1ce2ee
happysangitanms <happysangitanms@gmail.com>
2026-07-23 14:51:57 +0530

    Publish config.js with video ID for JOB-95

---
+++ MTG/CLASS-1/config.js
@@ -0,0 +1,10 @@
+const APP_CONFIG = {
+    sreni: "ଦଶମ",
+    subject: "ଜ୍ୟାମିତି"
+};
+const CLASS_NAME = "school10";
+const MEDIA_CONFIG = {
+    audio1: "https://audio.evidya.de/1MTG1.mp3",
+    video:  "https://www.youtube.com/embed/AkcQpEbau1g?playsinline=1&rel=0&modestbranding=1&autoplay=1",
+    pdf:    "https://audio.evidya.de/1MTG1.pdf"
+};
