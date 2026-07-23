043ba9949e0896747b391535e600352cfd61513e
happysangitanms <happysangitanms@gmail.com>
2026-07-23 13:21:12 +0530

    Update config.js

--- MTA/CLASS-1/config.js
+++ MTA/CLASS-1/config.js
@@ -5,6 +5,6 @@ const APP_CONFIG = {
 const CLASS_NAME = "school10";
 const MEDIA_CONFIG = {
     audio1: "https://audio.evidya.de/1MTA1.mp3",
-    video:  "https://www.youtube.com/embed/KlKsxJiBNwk?playsinline=1&rel=0&modestbranding=1&autoplay=1",
+    video:  "https://www.youtube.com/embed/UCa9JD1an2Q?playsinline=1&rel=0&modestbranding=1&autoplay=1",
     pdf:    "https://audio.evidya.de/1MTA1.pdf"
 };
