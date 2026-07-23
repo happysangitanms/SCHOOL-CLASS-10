78d4bd43dbccb289c3fc6ecd745623384b6d4eed
happysangitanms <happysangitanms@gmail.com>
2026-07-23 09:55:02 +0530

    Publish config.js with video ID for JOB-56

---
+++ SCL/CLASS-1/config.js
@@ -0,0 +1,10 @@
+const APP_CONFIG = {
+    sreni: "ଦଶମ",
+    subject: "ଜୀବ ବିଜ୍ଞାନ"
+};
+const CLASS_NAME = "school10";
+const MEDIA_CONFIG = {
+    audio1: "https://audio.evidya.de/1SCL1.mp3",
+    video:  "https://www.youtube.com/embed/xyWuLcQQFGU?playsinline=1&rel=0&modestbranding=1&autoplay=1",
+    pdf:    "https://audio.evidya.de/1SCL1.pdf"
+};
