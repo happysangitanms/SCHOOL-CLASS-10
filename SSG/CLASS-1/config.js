c706e9621d98699c6936fa4dba31c5395bd88748
happysangitanms <happysangitanms@gmail.com>
2026-07-23 09:09:08 +0530

    Publish config.js with video ID for JOB-49

---
+++ SSG/CLASS-1/config.js
@@ -0,0 +1,10 @@
+const APP_CONFIG = {
+    sreni: "ଦଶମ",
+    subject: "ଭୂଗୋଳ"
+};
+const CLASS_NAME = "school10";
+const MEDIA_CONFIG = {
+    audio1: "https://audio.evidya.de/1SSG1.mp3",
+    video:  "https://www.youtube.com/embed/undefined?playsinline=1&rel=0&modestbranding=1&autoplay=1",
+    pdf:    "https://audio.evidya.de/1SSG1.pdf"
+};
